ea4cc86f5947b3f03f10d1cd00cae7def50d4e8e
phoesco <78254702+phoesco@users.noreply.github.com>
2021-11-28 20:25:26 +0600

    Add files via upload

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountControler = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountControler,
+              // onChanged: (val) => amountInput = val,
+            ),
+            FlatButton(
+              onPressed: () {
+                print(titleController.text);
+              },
+              textColor: Colors.purple,
+              child: Text('Add Transaction'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
